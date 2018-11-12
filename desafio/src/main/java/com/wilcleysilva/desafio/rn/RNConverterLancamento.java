46976e92314f2f1e1460bc76f97373f067f8d313
wilcley.jadson.silva <wilcley.jadson.silva@CPX-FW44FVYP46Y.dir.svc.accenture.com>
2018-11-12 10:16:58 -0300

    refactor

---
+++ desafio/src/main/java/com/wilcleysilva/desafio/rn/RNConverterLancamento.java
@@ -0,0 +1,82 @@
+package com.wilcleysilva.desafio.rn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wilcleysilva.desafio.model.ExtratoLancamento;
+import com.wilcleysilva.desafio.model.LancamentoConta;
+import com.wilcleysilva.desafio.model.legado.DomicilioBancario;
+import com.wilcleysilva.desafio.model.legado.LancamentoContaCorrenteCliente;
+import com.wilcleysilva.desafio.model.legado.LancamentoLegado;
+import com.wilcleysilva.desafio.model.legado.ListaControleLancamento;
+
+public class RNConverterLancamento {
+
+	private static RNConverterLancamento aInstancia;
+
+	private RNConverterLancamento() {
+
+	}
+
+	public static RNConverterLancamento getInstancia() {
+
+		if (aInstancia == null) {
+			aInstancia = new RNConverterLancamento();
+		}
+
+		return aInstancia;
+	}
+
+	public ExtratoLancamento executar(LancamentoLegado pLancamentoLegado) {
+
+		ExtratoLancamento extratoLancamentoRetorno = new ExtratoLancamento();
+
+		List<LancamentoConta> lancamentos = new ArrayList<LancamentoConta>();
+
+		for (ListaControleLancamento controle : pLancamentoLegado.getListaControleLancamento()) {
+			LancamentoConta lancamentoConta = montarLancamentoConta(controle);
+
+			lancamentos.add(lancamentoConta);
+		}
+
+		extratoLancamentoRetorno.setLancamentosConta(lancamentos);
+
+		return extratoLancamentoRetorno;
+	}
+
+	private LancamentoConta montarLancamentoConta(ListaControleLancamento pControleLancamento) {
+		LancamentoConta lancamentoContaRetorno = new LancamentoConta();
+
+		if (pControleLancamento != null) {
+
+			lancamentoContaRetorno.setDtLancamento(pControleLancamento.getDataLancamentoContaCorrenteCliente());
+			lancamentoContaRetorno.setDtConfirmLancamento(pControleLancamento.getDataEfetivaLancamento());
+			lancamentoContaRetorno.setVlFinal(pControleLancamento.getValorLancamentoRemessa().toString());
+
+			LancamentoContaCorrenteCliente lancamentoContaCorrenteCliente = pControleLancamento
+					.getLancamentoContaCorrenteCliente();
+
+			if (lancamentoContaCorrenteCliente != null) {
+
+				lancamentoContaRetorno.setDsLancamento(lancamentoContaCorrenteCliente.getNomeTipoOperacao());
+				lancamentoContaRetorno
+						.setNuLancamento(lancamentoContaCorrenteCliente.getNumeroRemessaBanco().toString());
+				lancamentoContaRetorno.setSiLancamento(lancamentoContaCorrenteCliente.getNomeSituacaoRemessa());
+
+				DomicilioBancario domicilioBancario = lancamentoContaCorrenteCliente.getDadosDomicilioBancario();
+
+				if (domicilioBancario != null) {
+					String nomeBanco = (pControleLancamento.getNomeBanco() != null)
+							? pControleLancamento.getNomeBanco().trim()
+							: "";
+
+					lancamentoContaRetorno
+							.setDadosBancarios(nomeBanco.toUpperCase() + " Ag " + domicilioBancario.getNumeroAgencia()
+									+ " CC " + domicilioBancario.getNumeroContaCorrente());
+				}
+			}
+		}
+
+		return lancamentoContaRetorno;
+	}
+}
